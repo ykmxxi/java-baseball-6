d23147366161e6b6e1e335b505a46c048fa32874
ykmxxi <ykmxxi97@gmail.com>
2023-12-04 13:52:53 +0900

    feat(Balls): 중복된 숫자가 존재하는 경우 예외 처리 기능 구현

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -1,15 +1,32 @@
 package baseball.domain;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Balls {
 
+    private static final int BALLS_SIZE = 3;
+
     private final List<Ball> balls;
 
     public Balls(final String numbers) {
+        validate(numbers);
         this.balls = convertToBalls(numbers);
+    }
+
+    private void validate(final String numbers) {
+        if (getNumbersSizeWithoutDuplication(numbers) != BALLS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int getNumbersSizeWithoutDuplication(final String numbers) {
+        return Arrays.stream(numbers.split(""))
+                     .collect(Collectors.toSet())
+                     .size();
     }
 
     private List<Ball> convertToBalls(final String numbers) {
--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -1,11 +1,15 @@
 package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BallsTest {
 
@@ -20,6 +24,24 @@ class BallsTest {
 
         // then
         assertThat(result).contains(new Ball(1, 0), new Ball(2, 1), new Ball(3, 2));
+    }
+
+    @DisplayName("3개의 게임 숫자 예외 처리: 중복된 숫자가 존재하면 IllegalArgumentException 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"112", "121", "122"})
+    void 게임_숫자_3개_중복_예외_처리(String numbers) {
+        // when & then
+        assertThatThrownBy(() -> new Balls(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("3개의 게임 숫자 예외 처리 통과")
+    @ParameterizedTest
+    @ValueSource(strings = {"129", "931", "456"})
+    void 게임_숫자_3개_예외_처리_통과(String numbers) {
+        // when & then
+        assertThatCode(() -> new Balls(numbers))
+                .doesNotThrowAnyException();
     }
 
 }
